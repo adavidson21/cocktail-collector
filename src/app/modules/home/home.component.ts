eab270da39de45d359109bcec022bf15fceecf07
Alexis Davidson <alexis.davidson221@gmail.com>
2023-02-09 23:43:31 -0500

    Update popular drink list styling.

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { ViewEncapsulation } from '@angular/core';
 import { Drink, DrinkRoot } from 'src/app/models/drink';
@@ -10,20 +11,66 @@ import { HttpService } from 'src/app/shared/services/http.service';
   encapsulation: ViewEncapsulation.None,
 })
 export class HomeComponent {
+  /** The drinks. */
   public drinks: DrinkRoot | undefined;
 
+  /** The drinks that should be displayed. */
+  displayedDrinks: any;
+
+  /** The count of the drinks. */
+  dataCount: number;
+
+  /** The number of cards to be displayed at once. */
+  private numberOfCards: number = 4;
+
   constructor(private httpService: HttpService) {
-    this.getDrinks();
+    this.dataCount = 0;
+  }
+
+  ngOnInit() {
+    this.httpService.getDrinks().subscribe(
+      (data) => {
+        this.drinks = data as DrinkRoot;
+        this.setDisplayedDrinks();
+      },
+      (err: HttpErrorResponse) => {
+        console.log('Error parsing data: ' + err.message);
+      }
+    );
   }
 
   /**
-   * Gets the drinks.
+   * Sets the drinks to be displayed.
    */
-  async getDrinks() {
-    const response = await this.httpService.getDrinks();
+  setDisplayedDrinks() {
+    if (this.drinks) {
+      this.displayedDrinks = this.drinks.drinks.slice(
+        this.dataCount * this.numberOfCards,
+        (this.dataCount + 1) * this.numberOfCards
+      );
+    }
+  }
 
-    response.subscribe((data) => {
-      this.drinks = data as DrinkRoot;
-    });
+  /**
+   * Displays the drinks from the previous page.
+   */
+  previous() {
+    if (this.dataCount != 0) {
+      this.dataCount = this.dataCount - 1;
+      this.setDisplayedDrinks();
+    }
+  }
+
+  /**
+   * Displays the next set of drinks.
+   */
+  next() {
+    if (
+      this.drinks &&
+      (this.dataCount + 1) * this.numberOfCards < this.drinks.drinks.length
+    ) {
+      this.dataCount = this.dataCount + 1;
+      this.setDisplayedDrinks();
+    }
   }
 }
--- src/app/shared/services/http.service.ts
+++ src/app/shared/services/http.service.ts
@@ -9,18 +9,14 @@ export class HttpService {
   /** Base URL */
   private baseURL = environment.COCKTAIL_API_BASEURL;
 
-  constructor(private http: HttpClient) {
-    console.log('base', this.baseURL);
-  }
+  constructor(private http: HttpClient) {}
 
   /**
    * Gets the ingredients.
    * @returns The list of ingredients.
    */
   getIngredients() {
-    this.http.get(this.baseURL + 'list.php?i=list').subscribe((data) => {
-      return Object.values(data);
-    });
+    return this.http.get(this.baseURL + 'list.php?i=list');
   }
 
   /**
@@ -28,9 +24,7 @@ export class HttpService {
    * @returns The list of glasses.
    */
   getGlasses() {
-    this.http.get(this.baseURL + 'list.php?g=list').subscribe((data) => {
-      return Object.values(data);
-    });
+    return this.http.get(this.baseURL + 'list.php?g=list');
   }
 
   /**
@@ -38,9 +32,7 @@ export class HttpService {
    * @returns The list of categories.
    */
   getCategories() {
-    this.http.get(this.baseURL + 'list.php?c=list').subscribe((data) => {
-      return Object.values(data);
-    });
+    return this.http.get(this.baseURL + 'list.php?c=list');
   }
 
   /**
@@ -48,17 +40,15 @@ export class HttpService {
    * @returns The alcoholic filters list.
    */
   getAlcoholicFilters() {
-    this.http.get(this.baseURL + 'list.php?a=list').subscribe((data) => {
-      return Object.values(data);
-    });
+    return this.http.get(this.baseURL + 'list.php?a=list');
   }
 
   /**
    * Gets the recipes.
    * @returns The recipes.
    */
-  async getDrinks() {
+  getDrinks() {
     // TODO: Use random endpoint once patreon supporter. Temporarily use search by name for development.
-    return await this.http.get(this.baseURL + 'search.php?s=margarita');
+    return this.http.get(this.baseURL + 'search.php?s=margarita');
   }
 }
